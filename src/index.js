562119cb0c945cc35ad10375fcda618cff16f747
harshpreet14 <kaurharshpt6@gmail.com>
2023-07-05 00:35:33 +0530

    Setting Up Components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  return (<div className="container">
+            <Header/>
+            <Menu/>
+            <Footer/>
+          </div>);
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Menu(){
+  return(
+    <div className = "menu">
+      <main>Our Menu </main>
+      <Pizza
+      name = "Photo Spinaci"
+      ingredients = "Tomato, mushrooms"
+      photoName = "pizzas/spinaci.jpg"/>
+      <Pizza
+      name = "Photo Spinaci"
+      ingredients = "Tomato, mushrooms"
+      photoName = "pizzas/spinaci.jpg"/>
+      <Pizza
+      name = "Photo Spinaci"
+      ingredients = "Tomato, mushrooms"
+      photoName = "pizzas/spinaci.jpg"/>
+    </div>
+  )
+}
+
+function Pizza(props){
+  return (   
+    <div className='pizza'>
+      <h2>{props.name}</h2>
+    <img src = {props.photoName} alt={props.name}/>
+    <p>{props.ingredients}</p>
+    </div>
+    );           
+}
+
+function Header(){
+  return <header className="header">
+      <h1>Pizza Limited Co.</h1></header>
+}
+
+
+
+function Footer(){
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  //if(hour >= openHour && hour<= closeHour) 
+    //alert("We're currently open")
+  return (<footer className="footer">We're currently open</footer>);
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<React.StrictMode><App/></React.StrictMode>);
